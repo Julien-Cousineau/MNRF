a2a6788c8674edda1aab44997079a01ec8e95e67
Julien Cousineau <julien.cousineau@gmail.com>
2018-04-04 19:10:59 +0000

    change path

--- server/rastertiles.js
+++ server/rastertiles.js
@@ -2,6 +2,7 @@ const util = require('./util');
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+require('dotenv').config();
 const CLOUD=process.env.CLOUD;
 const ICETILES = path.resolve(CLOUD,'ice/tiles');
 
